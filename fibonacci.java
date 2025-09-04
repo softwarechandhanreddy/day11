d918cbab30df0313eb4f0c1f00e8ebea884f6e35
Hari Chandan Reddy <168167397+softwarechandhanreddy@users.noreply.github.com>
2025-09-04 16:49:52 +0530

    fibonacci

---
+++ fibonacci.java
@@ -0,0 +1,24 @@
+package com.codegnan.controlstatements;
+
+import java.util.Scanner;
+
+public class fibonacci {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int n = sc.nextInt();
+
+        int first = 0, second = 1;
+        System.out.print("Fibonacci Series: ");
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(first + " ");
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        sc.close();
+    }
+}
+
